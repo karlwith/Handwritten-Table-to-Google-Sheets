1629a6c2c9d65ec6778b2c354d8af62e50250648
Karl Withers <65951397+karlwith@users.noreply.github.com>
2020-07-21 15:00:41 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import os, io, string
+import os, io, string, gspread
 from google.cloud import vision
-from google.cloud.vision import types
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 # IMAGE PRE-PROCESSING shouldn't be needed with google vision but otherwise (scale to usable DPI, skew correction, adjust threshold to binarize, remove noise)
 
-def image_to_text():
+def image_to_text_lists():
 
-    # Login to google vision api
+    # Define google vision api credentials
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'VisionAPIKey.json'
     client = vision.ImageAnnotatorClient()
 
     # Define the image
     dirname = os.path.dirname(__file__)
-    file_path = os.path.join(dirname, r'Images\BuildBook01.jpg')
+    file_path = os.path.join(dirname, r'Images\datatable.jpg')
     with io.open(file_path, 'rb') as image_file:
         content = image_file.read()
 
@@ -21,7 +22,7 @@ def image_to_text():
     response = client.document_text_detection(image = input)
     return response
 
-def format_for_google_sheet(response):
+def format_for_google_sheet():
 
     # List recognised word blocks and ascociated confidence
     pages = response.full_text_annotation.pages
@@ -44,45 +45,54 @@ def format_for_google_sheet(response):
         split = text.split()
         joinedString = " ".join(split)
 
-    # Break into separate lists of date, bikes, serials with ':'
+    # Break into separate lists of date, bikes, serials 
     joinedList = joinedString.split(':')
     date = joinedList[0]
     bikes = joinedList[1]    
     serials = joinedList[2]
 
-    # Substitute brand names for shorthand and separate bike lines
+    # Parse bike lines
     bikes = bikes.replace('R ', '\nReid ')
     bikes = bikes.replace('M ', '\nMerida ')
     bikes = bikes.replace('N ', '\nNorco ')
     bikes = bikes.strip()
     bikes = bikes.split('\n')
     
-    # Separate serial lines with '+'   
+    # Parse serial lines
     serials = serials.replace(" ", "")
     serials = serials.split('+')
     
-    # Assign date for each line
+    # Assign date and builder for each line
     date = date.strip()
     dates = [date] * len(bikes)
-
-    # Assign builder for each line
     builder = 'Karl'
     builder = [builder] * len(bikes)
 
-    print(dates)
-    print(bikes)
-    print(serials)
-    print(builder)
+    return dates, bikes, serials, builder 
 
 def upload_to_google_sheet():
 
-    # Integrate google sheets api and upload lists at next available row in the correct sheet
-    placeholder = 1
-
+    # Define google sheet api credentials
+    scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("DriveAPIKey.json", scope)
+    client = gspread.authorize(creds)
+      
+    # Insert new row for each list index
+    index = 0
+    sheet = client.open("buildtracker").sheet1
+    for (i, j, k, z) in zip(dates, bikes, serials, builder):
+        data = sheet.get_all_records()
+        nextRow = [dates[index], bikes[index], serials[index], builder[index]]
+        sheet.insert_row(nextRow, len(data)+2)
+        index += 1
+    
 if __name__ == "__main__":
 
     # Send image to google and return response
-    response = image_to_text()
+    response = image_to_text_lists()
 
     # Format response for google sheet
-    format_for_google_sheet(response)
+    dates, bikes, serials, builder = format_for_google_sheet()
+
+    # Upload to google sheet
+    upload_to_google_sheet()
